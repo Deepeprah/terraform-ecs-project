740053512971bb6fdf944d2c27a97dc03af51c57
Dee <deebio2@yahoo.co.uk>
2023-03-22 07:56:17 +0000

    files

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "elearning-vpc" {
 
 # create internet gateway and attach it to vpc
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.elearning-vpc.id 
+  vpc_id = aws_vpc.elearning-vpc.id
 
   tags = {
     Name = "${var.project_name}-${var.environment}-igw"
@@ -23,10 +23,10 @@ data "aws_availability_zones" "available_zones" {}
 
 # create public subnet az1
 resource "aws_subnet" "public_subnet_az1" {
-  vpc_id                  = aws_vpc.elearning-vpc.id  
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.public_subnet_az1_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[0] 
-  map_public_ip_on_launch = true 
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.project_name}-${var.environment}-public-az1"
@@ -35,9 +35,9 @@ resource "aws_subnet" "public_subnet_az1" {
 
 # create public subnet az2
 resource "aws_subnet" "public_subnet_az2" {
-  vpc_id                  = aws_vpc.elearning-vpc.id  
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.public_subnet_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1] 
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = true
 
   tags = {
@@ -47,7 +47,7 @@ resource "aws_subnet" "public_subnet_az2" {
 
 # create route table and add public route
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.elearning-vpc.id 
+  vpc_id = aws_vpc.elearning-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -61,21 +61,21 @@ resource "aws_route_table" "public_route_table" {
 
 # associate public subnet az1 to "public route table"
 resource "aws_route_table_association" "public_subnet_az1_rt_association" {
-  subnet_id      = aws_subnet.public_subnet_az1.id  
-  route_table_id = aws_route_table.public_route_table.id 
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # associate public subnet az2 to "public route table"
 resource "aws_route_table_association" "public_subnet_2_rt_association" {
-  subnet_id      = aws_subnet.public_subnet_az2.id 
-  route_table_id = aws_route_table.public_route_table.id 
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # create private app subnet az1
 resource "aws_subnet" "private_app_subnet_az1" {
-  vpc_id                  = aws_vpc.elearning-vpc.id  
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.private_app_subnet_az1_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[0]  
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
   map_public_ip_on_launch = false
 
   tags = {
@@ -85,9 +85,9 @@ resource "aws_subnet" "private_app_subnet_az1" {
 
 # create private app subnet az2
 resource "aws_subnet" "private_app_subnet_az2" {
-  vpc_id                  = aws_vpc.elearning-vpc.id 
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.private_app_subnet_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1]  
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = false
 
   tags = {
@@ -97,9 +97,9 @@ resource "aws_subnet" "private_app_subnet_az2" {
 
 # create private data subnet az1
 resource "aws_subnet" "private_data_subnet_az1" {
-  vpc_id                  = aws_vpc.elearning-vpc.id  
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.private_data_subnet_az1_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[0] 
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
   map_public_ip_on_launch = false
 
   tags = {
@@ -111,7 +111,7 @@ resource "aws_subnet" "private_data_subnet_az1" {
 resource "aws_subnet" "private_data_subnet_az2" {
   vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.private_data_subnet_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1]  
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = false
 
   tags = {
